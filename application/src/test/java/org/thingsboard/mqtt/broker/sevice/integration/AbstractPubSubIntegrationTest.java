f0325566444fe622a2d5dab1bd9ab97768ea3f36
vzikratyi <vzikratyi@thingsboard.io>
2020-12-28 10:51:16 +0200

    Refactored integration tests

--- application/src/test/java/org/thingsboard/mqtt/broker/sevice/integration/AbstractPubSubIntegrationTest.java
+++ application/src/test/java/org/thingsboard/mqtt/broker/sevice/integration/AbstractPubSubIntegrationTest.java
@@ -19,7 +19,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.jodah.concurrentunit.Waiter;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
@@ -36,55 +35,14 @@ import org.testcontainers.utility.DockerImageName;
 import org.thingsboard.mqtt.broker.ThingsboardMQTTBrokerApplication;
 import org.thingsboard.mqtt.broker.queue.kafka.settings.PublishMsgKafkaSettings;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 @Import(AbstractPubSubIntegrationTest.KafkaTestContainersConfiguration.class)
 @ComponentScan({"org.thingsboard.mqtt.broker"})
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @SpringBootTest(classes = ThingsboardMQTTBrokerApplication.class)
 public abstract class AbstractPubSubIntegrationTest {
-    static final int SUBSCRIBERS_COUNT = 10;
-    static final int PUBLISHERS_COUNT = 5;
-    static final int PUBLISH_MSGS_COUNT = 100;
-
     @ClassRule
     public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
-
-    void initPubSubTest() throws Throwable {
-        Waiter subscribersWaiter = new Waiter();
-        CountDownLatch connectingSubscribers = new CountDownLatch(SUBSCRIBERS_COUNT);
-        ExecutorService executor = Executors.newFixedThreadPool(PUBLISHERS_COUNT);
-        for (int i = 0; i < SUBSCRIBERS_COUNT; i++) {
-            int finalI = i;
-            executor.execute(() -> {
-                initSubscriber(subscribersWaiter, finalI);
-                connectingSubscribers.countDown();
-            });
-        }
-
-        connectingSubscribers.await(2, TimeUnit.SECONDS);
-
-        CountDownLatch processingPublishers = new CountDownLatch(PUBLISHERS_COUNT);
-        for (int i = 0; i < PUBLISHERS_COUNT; i++) {
-            int finalI = i;
-            executor.execute(() -> {
-                initPublisher(subscribersWaiter, finalI);
-                processingPublishers.countDown();
-            });
-        }
-
-        processingPublishers.await(2, TimeUnit.SECONDS);
-
-        subscribersWaiter.await(1, TimeUnit.SECONDS, SUBSCRIBERS_COUNT);
-    }
-
-    abstract void initSubscriber(Waiter waiter, int subscriberId);
-
-    abstract void initPublisher(Waiter waiter, int publisherId);
 
     @AllArgsConstructor
     @NoArgsConstructor
--- application/src/test/java/org/thingsboard/mqtt/broker/sevice/integration/TopicsIntegrationTest.java
+++ application/src/test/java/org/thingsboard/mqtt/broker/sevice/integration/TopicsIntegrationTest.java
@@ -21,6 +21,7 @@ import net.jodah.concurrentunit.Waiter;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.test.annotation.DirtiesContext;
@@ -28,13 +29,26 @@ import org.springframework.test.context.ContextConfiguration;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
 @Slf4j
-@DirtiesContext
-@ContextConfiguration(classes = SingleLevelTopicPubSubIntegrationTest.class, loader = SpringBootContextLoader.class)
-public class SingleLevelTopicPubSubIntegrationTest extends AbstractPubSubIntegrationTest {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ContextConfiguration(classes = TopicsIntegrationTest.class, loader = SpringBootContextLoader.class)
+public class TopicsIntegrationTest extends AbstractPubSubIntegrationTest {
+
+    private static final String BASIC_TOPIC = "use-case/country/city/store/department/group/device";
+
+    private static final String MULTI_LEVEL_TOPIC_FILTER = "use-case/country/city/#";
+    private static final List<String> MULTI_LEVEL_TOPICS = Arrays.asList(
+            "use-case/country/city/store",
+            "use-case/country/city/store/department/group",
+            "use-case/country/city/store/department/1",
+            "use-case/country/city/store/group/2",
+            "use-case/country/city/test/1"
+    );
+
     private static final String SINGLE_LEVELS_TOPIC_FILTER = "use-case/+/city/store/department/group/+";
-    private static final List<String> TOPICS = Arrays.asList(
+    private static final List<String> SINGLE_LEVELS_TOPICS = Arrays.asList(
             "use-case/ua/city/store/department/group/1",
             "use-case/ua/city/store/department/group/2",
             "use-case/uk/city/store/department/group/1",
@@ -49,23 +63,41 @@ public class SingleLevelTopicPubSubIntegrationTest extends AbstractPubSubIntegra
 
     private final ObjectMapper mapper = new ObjectMapper();
 
+    @Autowired
+    private IntegrationTestInitService initPubSubTestService;
+
     @Test
-    public void testPubSubSingleLevelTopic() throws Throwable {
-        initPubSubTest();
+    public void testPubSubBasicTopic() throws Throwable {
+        initPubSubTestService.initPubSubTest(
+                (waiter, subscriberId) -> initSubscriber(waiter, subscriberId, BASIC_TOPIC),
+                (waiter, publisherId) -> initPublisher(waiter, publisherId, () -> BASIC_TOPIC));
     }
 
-    @Override
-    void initPublisher(Waiter waiter, int publisherId) {
+    @Test
+    public void testPubSubSingleLevelsTopic() throws Throwable {
+        initPubSubTestService.initPubSubTest(
+                (waiter, subscriberId) -> initSubscriber(waiter, subscriberId, SINGLE_LEVELS_TOPIC_FILTER),
+                (waiter, publisherId) -> initPublisher(waiter, publisherId, () -> SINGLE_LEVELS_TOPICS.get(publisherId % SINGLE_LEVELS_TOPICS.size())));
+    }
+
+    @Test
+    public void testPubSubMultipleLevelsTopic() throws Throwable {
+        initPubSubTestService.initPubSubTest(
+                (waiter, subscriberId) -> initSubscriber(waiter, subscriberId, MULTI_LEVEL_TOPIC_FILTER),
+                (waiter, publisherId) -> initPublisher(waiter, publisherId, () -> MULTI_LEVEL_TOPICS.get(publisherId % MULTI_LEVEL_TOPICS.size())));
+    }
+
+    void initPublisher(Waiter waiter, int publisherId, Supplier<String> topicSupplier) {
         try {
-            MqttClient pubClient = new MqttClient("tcp://" + mqttAddress + ":" + mqttPort, "single_level_topic_pub_client_" + publisherId);
+            MqttClient pubClient = new MqttClient("tcp://" + mqttAddress + ":" + mqttPort, "topic_pub_client_"
+                    + publisherId + "_" + UUID.randomUUID().toString());
             pubClient.connect();
-            String topic = TOPICS.get(publisherId % TOPICS.size());
-            for (int j = 0; j < PUBLISH_MSGS_COUNT; j++) {
+            for (int j = 0; j < IntegrationTestInitService.PUBLISH_MSGS_COUNT; j++) {
                 MqttMessage msg = new MqttMessage();
-                TestPublishMsg payload = new TestPublishMsg(publisherId, j, j == PUBLISH_MSGS_COUNT - 1);
+                TestPublishMsg payload = new TestPublishMsg(publisherId, j, j == IntegrationTestInitService.PUBLISH_MSGS_COUNT - 1);
                 msg.setPayload(mapper.writeValueAsBytes(payload));
                 msg.setQos(j % 2);
-                pubClient.publish(topic, msg);
+                pubClient.publish(topicSupplier.get(), msg);
             }
             log.info("[{}] Publisher stopped publishing", publisherId);
         } catch (Exception e) {
@@ -75,15 +107,15 @@ public class SingleLevelTopicPubSubIntegrationTest extends AbstractPubSubIntegra
         }
     }
 
-    @Override
-    void initSubscriber(Waiter waiter, int subscriberId) {
+    void initSubscriber(Waiter waiter, int subscriberId, String topicFilter) {
         try {
-            MqttClient subClient = new MqttClient("tcp://" + mqttAddress + ":" + mqttPort, "single_level_topic_sub_client_" + subscriberId);
+            MqttClient subClient = new MqttClient("tcp://" + mqttAddress + ":" + mqttPort, "topic_sub_client_"
+                    + subscriberId + "_" + UUID.randomUUID().toString());
             subClient.connect();
             Map<Integer, TestPublishMsg> previousMsgs = new HashMap<>();
-            AtomicInteger unfinishedProducers = new AtomicInteger(PUBLISHERS_COUNT);
+            AtomicInteger unfinishedProducers = new AtomicInteger(IntegrationTestInitService.PUBLISHERS_COUNT);
             int qos = subscriberId % 2;
-            subClient.subscribe(SINGLE_LEVELS_TOPIC_FILTER, qos, (topic, message) -> {
+            subClient.subscribe(topicFilter, qos, (topic, message) -> {
                 TestPublishMsg currentMsg = mapper.readValue(message.getPayload(), TestPublishMsg.class);
                 TestPublishMsg previousMsg = previousMsgs.get(currentMsg.publisherId);
                 if (previousMsg != null) {
