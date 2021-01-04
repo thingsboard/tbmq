812b344c37a2c655c4dc18161edac85a677b4404
vzikratyi <vzikratyi@thingsboard.io>
2021-01-04 15:41:31 +0200

    Fixed MqttClientCredentialsServiceTest

--- dao/src/test/java/org/thingsboard/mqtt/broker/dao/service/MqttClientCredentialsServiceTest.java
+++ dao/src/test/java/org/thingsboard/mqtt/broker/dao/service/MqttClientCredentialsServiceTest.java
@@ -12,10 +12,7 @@ import org.thingsboard.mqtt.broker.dao.client.MqttClientCredentialsService;
 import org.thingsboard.mqtt.broker.dao.exception.DataValidationException;
 import org.thingsboard.mqtt.broker.dao.util.protocol.ProtocolUtil;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 @DaoSqlTest
 public class MqttClientCredentialsServiceTest extends AbstractServiceTest {
@@ -48,92 +45,69 @@ public class MqttClientCredentialsServiceTest extends AbstractServiceTest {
 
     @Test(expected = DataValidationException.class)
     public void testCreateDuplicateCredentials() throws JsonProcessingException {
-        MqttClientCredentials clientCredentials = null;
+        MqttClientCredentials clientCredentials = mqttClientCredentialsService.saveCredentials(validMqttClientCredentials("client", "user", null));
         try {
-            clientCredentials = mqttClientCredentialsService.saveCredentials(validMqttClientCredentials("client", "user", null));
             mqttClientCredentialsService.saveCredentials(validMqttClientCredentials("client", "user", "password"));
         } finally {
-            if (clientCredentials != null) {
-                mqttClientCredentialsService.deleteCredentials(clientCredentials.getId());
-            }
+            mqttClientCredentialsService.deleteCredentials(clientCredentials.getId());
         }
     }
 
     @Test
     public void testFindMatchingMixed() throws JsonProcessingException {
         MqttClientCredentials client1Credentials = null, client2Credentials = null, client3Credentials = null;
-        try {
-            client1Credentials = mqttClientCredentialsService.saveCredentials(
-                    validMqttClientCredentials("client1", "test1", "password1"));
-            client2Credentials = mqttClientCredentialsService.saveCredentials(
-                    validMqttClientCredentials("client2", "test1", null));
-            client3Credentials = mqttClientCredentialsService.saveCredentials(
-                    validMqttClientCredentials("client1", "test2", null));
+        client1Credentials = mqttClientCredentialsService.saveCredentials(
+                validMqttClientCredentials("client1", "test1", "password1"));
+        client2Credentials = mqttClientCredentialsService.saveCredentials(
+                validMqttClientCredentials("client2", "test1", null));
+        client3Credentials = mqttClientCredentialsService.saveCredentials(
+                validMqttClientCredentials("client1", "test2", null));
 
-            Assert.assertEquals(
-                    Collections.singletonList(client1Credentials),
-                    mqttClientCredentialsService.findMatchingCredentials(Collections.singletonList(
-                            ProtocolUtil.mixedCredentialsId("test1", "client1")
-                    )));
-        } finally {
-            if (client1Credentials != null) {
-                mqttClientCredentialsService.deleteCredentials(client1Credentials.getId());
-            }
-            if (client2Credentials != null) {
-                mqttClientCredentialsService.deleteCredentials(client2Credentials.getId());
-            }
-            if (client3Credentials != null) {
-                mqttClientCredentialsService.deleteCredentials(client3Credentials.getId());
-            }
-        }
+        Assert.assertEquals(
+                Collections.singletonList(client1Credentials),
+                mqttClientCredentialsService.findMatchingCredentials(Collections.singletonList(
+                        ProtocolUtil.mixedCredentialsId("test1", "client1")
+                )));
+
+        mqttClientCredentialsService.deleteCredentials(client1Credentials.getId());
+        mqttClientCredentialsService.deleteCredentials(client2Credentials.getId());
+        mqttClientCredentialsService.deleteCredentials(client3Credentials.getId());
     }
 
     @Test
     public void testFindMatchingByUserName() throws JsonProcessingException {
         MqttClientCredentials client1Credentials = null, client2Credentials = null;
-        try {
-            client1Credentials = mqttClientCredentialsService.saveCredentials(
-                    validMqttClientCredentials(null, "user1", null));
-            client2Credentials = mqttClientCredentialsService.saveCredentials(
-                    validMqttClientCredentials(null, "user2", null));
+        client1Credentials = mqttClientCredentialsService.saveCredentials(
+                validMqttClientCredentials(null, "user1", null));
+        client2Credentials = mqttClientCredentialsService.saveCredentials(
+                validMqttClientCredentials(null, "user2", null));
 
-            Assert.assertEquals(
-                    Collections.singletonList(client1Credentials),
-                    mqttClientCredentialsService.findMatchingCredentials(Collections.singletonList(
-                            ProtocolUtil.usernameCredentialsId("user1")
-                    )));
-        } finally {
-            if (client1Credentials != null) {
-                mqttClientCredentialsService.deleteCredentials(client1Credentials.getId());
-            }
-            if (client2Credentials != null) {
-                mqttClientCredentialsService.deleteCredentials(client2Credentials.getId());
-            }
-        }
+        Assert.assertEquals(
+                Collections.singletonList(client1Credentials),
+                mqttClientCredentialsService.findMatchingCredentials(Collections.singletonList(
+                        ProtocolUtil.usernameCredentialsId("user1")
+                )));
+
+        mqttClientCredentialsService.deleteCredentials(client1Credentials.getId());
+        mqttClientCredentialsService.deleteCredentials(client2Credentials.getId());
     }
 
     @Test
     public void testFindMatchingByClientId() throws JsonProcessingException {
         MqttClientCredentials client1Credentials = null, client2Credentials = null;
-        try {
-            client1Credentials = mqttClientCredentialsService.saveCredentials(
-                    validMqttClientCredentials("client1", null, null));
-            client2Credentials = mqttClientCredentialsService.saveCredentials(
-                    validMqttClientCredentials("client2", null, null));
+        client1Credentials = mqttClientCredentialsService.saveCredentials(
+                validMqttClientCredentials("client1", null, null));
+        client2Credentials = mqttClientCredentialsService.saveCredentials(
+                validMqttClientCredentials("client2", null, null));
 
-            Assert.assertEquals(
-                    Collections.singletonList(client1Credentials),
-                    mqttClientCredentialsService.findMatchingCredentials(Collections.singletonList(
-                            ProtocolUtil.clientIdCredentialsId("client1")
-                    )));
-        } finally {
-            if (client1Credentials != null) {
-                mqttClientCredentialsService.deleteCredentials(client1Credentials.getId());
-            }
-            if (client2Credentials != null) {
-                mqttClientCredentialsService.deleteCredentials(client2Credentials.getId());
-            }
-        }
+        Assert.assertEquals(
+                Collections.singletonList(client1Credentials),
+                mqttClientCredentialsService.findMatchingCredentials(Collections.singletonList(
+                        ProtocolUtil.clientIdCredentialsId("client1")
+                )));
+
+        mqttClientCredentialsService.deleteCredentials(client1Credentials.getId());
+        mqttClientCredentialsService.deleteCredentials(client2Credentials.getId());
     }
 
     private MqttClientCredentials validMqttClientCredentials(String clientId, String username, String password) throws JsonProcessingException {
