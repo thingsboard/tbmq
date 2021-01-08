3d6f08c33489f89a6d81dba48acd4d0584c79eef
vzikratyi <vzikratyi@thingsboard.io>
2021-01-08 12:35:21 +0200

    Added swagger config

---
+++ application/src/main/java/org/thingsboard/mqtt/broker/config/SwaggerConfiguration.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright © 2016-2020 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.mqtt.broker.config;
+
+import com.fasterxml.classmate.ResolvedType;
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thingsboard.mqtt.broker.common.data.security.Authority;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.schema.AlternateTypeRule;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+import static com.google.common.base.Predicates.and;
+import static com.google.common.base.Predicates.not;
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Value("${swagger.api_path_regex}")
+    private String apiPathRegex;
+    @Value("${swagger.security_path_regex}")
+    private String securityPathRegex;
+    @Value("${swagger.non_security_path_regex}")
+    private String nonSecurityPathRegex;
+    @Value("${swagger.title}")
+    private String title;
+    @Value("${swagger.description}")
+    private String description;
+    @Value("${swagger.contact.name}")
+    private String contactName;
+    @Value("${swagger.contact.url}")
+    private String contactUrl;
+    @Value("${swagger.contact.email}")
+    private String contactEmail;
+    @Value("${swagger.license.title}")
+    private String licenseTitle;
+    @Value("${swagger.license.url}")
+    private String licenseUrl;
+    @Value("${swagger.version}")
+    private String version;
+
+    @Bean
+    public Docket thingsboardMqttBrokerApi() {
+        TypeResolver typeResolver = new TypeResolver();
+        final ResolvedType jsonNodeType =
+                typeResolver.resolve(
+                        JsonNode.class);
+        final ResolvedType stringType =
+                typeResolver.resolve(
+                        String.class);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("thingsboard-mqtt-broker")
+                .apiInfo(apiInfo())
+                .alternateTypeRules(
+                        new AlternateTypeRule(
+                                jsonNodeType,
+                                stringType))
+                .select()
+                .paths(apiPaths())
+                .build()
+                .securitySchemes(newArrayList(jwtTokenKey()))
+                .securityContexts(newArrayList(securityContext()))
+                .enableUrlTemplating(true);
+    }
+
+    private ApiKey jwtTokenKey() {
+        return new ApiKey("X-Authorization", "JWT token", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(securityPaths())
+                .build();
+    }
+
+    private Predicate<String> apiPaths() {
+        return regex(apiPathRegex);
+    }
+
+    private Predicate<String> securityPaths() {
+        return and(
+                regex(securityPathRegex),
+                not(regex(nonSecurityPathRegex))
+        );
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = new AuthorizationScope(Authority.ADMIN.name(), "Administrator");
+        return newArrayList(
+                new SecurityReference("X-Authorization", authorizationScopes));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .description(description)
+                .contact(new Contact(contactName, contactUrl, contactEmail))
+                .license(licenseTitle)
+                .licenseUrl(licenseUrl)
+                .version(version)
+                .build();
+    }
+
+}
