c08872327fe25b700537afd65277f8e4a42e2b4f
dlandiak <dlandiak@thingsboard.io>
2022-11-25 12:03:50 +0200

    tests fix

--- dao/src/test/resources/sql/system-data.sql
+++ dao/src/test/resources/sql/system-data.sql
@@ -17,9 +17,28 @@
 /** SYSTEM **/
 
 /** Admin **/
-INSERT INTO broker_user ( id, created_time, email, authority )
-VALUES ( '5a797660-4612-11e7-a919-92ebcb67fe33', 1592576748000, 'sysadmin@thingsboard.org', 'SYS_ADMIN' );
+INSERT INTO broker_user (id, created_time, email, authority)
+VALUES ('5a797660-4612-11e7-a919-92ebcb67fe33', 1592576748000, 'sysadmin@thingsboard.org', 'SYS_ADMIN');
 
-INSERT INTO user_credentials ( id, created_time, user_id, enabled, password )
-VALUES ( '61441950-4612-11e7-a919-92ebcb67fe33', 1592576748000, '5a797660-4612-11e7-a919-92ebcb67fe33', true,
-         '$2a$10$5JTB8/hxWc9WAy62nCGSxeefl3KWmipA9nFpVdDa0/xfIseeBB4Bu' );
+INSERT INTO user_credentials (id, created_time, user_id, enabled, password)
+VALUES ('61441950-4612-11e7-a919-92ebcb67fe33', 1592576748000, '5a797660-4612-11e7-a919-92ebcb67fe33', true,
+        '$2a$10$5JTB8/hxWc9WAy62nCGSxeefl3KWmipA9nFpVdDa0/xfIseeBB4Bu');
+
+/** System settings **/
+INSERT INTO admin_settings (id, created_time, key, json_value)
+VALUES ('6a2266e4-4612-11e7-a919-92ebcb67fe33', 1592576748000, 'general', '{
+	"baseUrl": "http://localhost:8083"
+}');
+
+INSERT INTO admin_settings (id, created_time, key, json_value)
+VALUES ('6eaaefa6-4612-11e7-a919-92ebcb67fe33', 1592576748000, 'mail', '{
+	"mailFrom": "Thingsboard <sysadmin@localhost.localdomain>",
+	"smtpProtocol": "smtp",
+	"smtpHost": "localhost",
+	"smtpPort": "25",
+	"timeout": "10000",
+	"enableTls": false,
+	"tlsVersion": "TLSv1.2",
+	"username": "",
+	"password": ""
+}');
