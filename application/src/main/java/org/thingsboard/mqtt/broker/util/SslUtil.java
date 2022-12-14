1a0a38e5ebdaf449a57863f67f1651f09347e1bc
dlandiak <dlandiak@thingsboard.io>
2022-12-14 18:06:07 +0200

    pem support

--- application/src/main/java/org/thingsboard/mqtt/broker/util/SslUtil.java
+++ application/src/main/java/org/thingsboard/mqtt/broker/util/SslUtil.java
@@ -20,15 +20,23 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x500.style.IETFUtils;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import org.springframework.util.Base64Utils;
 
+import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 
 public class SslUtil {
+
     public static String parseCommonName(X509Certificate certificate) throws CertificateEncodingException {
         X500Name x500name = new JcaX509CertificateHolder(certificate).getSubject();
         RDN cn = x500name.getRDNs(BCStyle.CN)[0];
 
         return IETFUtils.valueToString(cn.getFirst().getValue());
+    }
+
+    public static String getCertificateString(Certificate cert)
+            throws CertificateEncodingException {
+        return EncryptionUtil.certTrimNewLines(Base64Utils.encodeToString(cert.getEncoded()));
     }
 }
