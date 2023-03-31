8c9313978155eb711ecf2a93929bec0af3ed4a6b
deaflynx <deaflynx@gmail.com>
2023-03-31 13:33:48 +0300

    Home page fix background color

--- ui-ngx/src/app/modules/home/components/kafka-tables-tab-group/kafka-tables-tab-group.component.ts
+++ ui-ngx/src/app/modules/home/components/kafka-tables-tab-group/kafka-tables-tab-group.component.ts
@@ -15,7 +15,7 @@
 ///
 
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from "@ngx-translate/core";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'tb-kafka-tables-tab-group',
@@ -24,17 +24,18 @@ import { TranslateService } from "@ngx-translate/core";
 })
 export class KafkaTablesTabGroupComponent implements OnInit {
 
-  constructor(private translate: TranslateService) { }
+  constructor(private translate: TranslateService) {
+  }
 
   ngOnInit(): void {
   }
 
   label(tab: 'topics' | 'consumerGroups') {
     switch (tab) {
-      case "topics":
-        return this.translate.instant('kafka.topics')
-      case "consumerGroups":
-        return this.translate.instant('kafka.consumer-groups')
+      case 'topics':
+        return this.translate.instant('kafka.topics');
+      case 'consumerGroups':
+        return this.translate.instant('kafka.consumer-groups');
     }
   }
 
