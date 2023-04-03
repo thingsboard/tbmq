a398f391779f3edb60a2430328652ed374a1e27e
deaflynx <deaflynx@gmail.com>
2023-04-03 15:41:27 +0300

    Home page make cards lighter

--- ui-ngx/src/app/modules/home/components/kafka-tables-tab-group/kafka-tables-tab-group.component.ts
+++ ui-ngx/src/app/modules/home/components/kafka-tables-tab-group/kafka-tables-tab-group.component.ts
@@ -14,29 +14,11 @@
 /// limitations under the License.
 ///
 
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tb-kafka-tables-tab-group',
   templateUrl: './kafka-tables-tab-group.component.html',
   styleUrls: ['./kafka-tables-tab-group.component.scss']
 })
-export class KafkaTablesTabGroupComponent implements OnInit {
-
-  constructor(private translate: TranslateService) {
-  }
-
-  ngOnInit(): void {
-  }
-
-  label(tab: 'topics' | 'consumerGroups') {
-    switch (tab) {
-      case 'topics':
-        return this.translate.instant('kafka.topics');
-      case 'consumerGroups':
-        return this.translate.instant('kafka.consumer-groups');
-    }
-  }
-
-}
+export class KafkaTablesTabGroupComponent {}
