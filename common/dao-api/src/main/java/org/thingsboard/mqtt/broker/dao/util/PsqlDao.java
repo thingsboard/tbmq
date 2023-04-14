4b05fc979fc5a920b748784fc2411c2b7375de97
Dima Landiak <dlandiak@thingsboard.io>
2023-04-14 15:13:28 +0300

    drop hsqldb support

--- common/dao-api/src/main/java/org/thingsboard/mqtt/broker/dao/util/PsqlDao.java
+++ common/dao-api/src/main/java/org/thingsboard/mqtt/broker/dao/util/PsqlDao.java
@@ -19,6 +19,5 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-//@ConditionalOnProperty(prefix = "spring.jpa", value = "database-platform", havingValue = "org.hibernate.dialect.PostgreSQLDialect")
 public @interface PsqlDao {
 }
