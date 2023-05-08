e0f939ad97a78baa1d2ac6db691362801a5aa35d
Dima Landiak <dlandiak@thingsboard.io>
2023-05-08 12:47:23 +0300

    added empty lines at the end of files

--- dao/src/main/java/org/thingsboard/mqtt/broker/dao/sqlts/insert/AbstractInsertRepository.java
+++ dao/src/main/java/org/thingsboard/mqtt/broker/dao/sqlts/insert/AbstractInsertRepository.java
@@ -16,32 +16,16 @@
 package org.thingsboard.mqtt.broker.dao.sqlts.insert;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import java.util.regex.Pattern;
-
 @Repository
 public abstract class AbstractInsertRepository {
-
-    private static final ThreadLocal<Pattern> PATTERN_THREAD_LOCAL = ThreadLocal.withInitial(() -> Pattern.compile(String.valueOf(Character.MIN_VALUE)));
-    private static final String EMPTY_STR = "";
-
-    @Value("${sql.remove_null_chars:true}")
-    private boolean removeNullChars;
 
     @Autowired
     protected JdbcTemplate jdbcTemplate;
 
     @Autowired
     protected TransactionTemplate transactionTemplate;
-
-    protected String replaceNullChars(String strValue) {
-        if (removeNullChars && strValue != null) {
-            return PATTERN_THREAD_LOCAL.get().matcher(strValue).replaceAll(EMPTY_STR);
-        }
-        return strValue;
-    }
 }
