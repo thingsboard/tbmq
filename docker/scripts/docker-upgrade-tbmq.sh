d2327f716adc780a335fa5474700722c4d7372ce
Dima Landiak <dlandiak@thingsboard.io>
2023-08-21 16:43:53 +0300

    upgrade script for tbmq-node image for docker compose

---
+++ docker/scripts/docker-upgrade-tbmq.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright © 2016-2023 The Thingsboard Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+for i in "$@"
+do
+case $i in
+    --fromVersion=*)
+    FROM_VERSION="${i#*=}"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+if [[ -z "${FROM_VERSION// }" ]]; then
+    echo "--fromVersion parameter is invalid or unspecified!"
+    echo "Usage: docker-upgrade-tb.sh --fromVersion={VERSION}"
+    exit 1
+else
+    fromVersion="${FROM_VERSION// }"
+fi
+
+set -e
+
+docker compose -f docker-compose.yml pull tb-mqtt-broker-1
+
+docker compose -f docker-compose.yml up -d postgres
+
+docker compose -f docker-compose.yml run --no-deps --rm \
+-e UPGRADE_TB=true -e FROM_VERSION=${fromVersion} tb-mqtt-broker-1
