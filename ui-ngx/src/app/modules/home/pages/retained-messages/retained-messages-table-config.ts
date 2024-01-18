e3c913432b2d817f3ea8aa022a8c978174561cea
deaflynx <deaflynx@gmail.com>
2024-01-18 12:35:02 +0200

    Update QoS translation map in RetainedMessagesTableConfig

--- ui-ngx/src/app/modules/home/pages/retained-messages/retained-messages-table-config.ts
+++ ui-ngx/src/app/modules/home/pages/retained-messages/retained-messages-table-config.ts
@@ -35,7 +35,7 @@ import {
 } from '@home/components/event/event-content-dialog.component';
 import { ContentType } from '@shared/models/constants';
 import { MatDialog } from '@angular/material/dialog';
-import { QoSTranslationMap } from '@shared/models/session.model';
+import { WsQoSTranslationMap } from '@shared/models/session.model';
 import { isDefinedAndNotNull } from '@core/utils';
 
 export class RetainedMessagesTableConfig extends EntityTableConfig<RetainedMessage> {
@@ -94,9 +94,9 @@ export class RetainedMessagesTableConfig extends EntityTableConfig<RetainedMessa
           onAction: ($event, entity) => entity.topic,
           type: CellActionDescriptorType.COPY_BUTTON
         }),
-      new EntityTableColumn<RetainedMessage>('qos', 'retained-message.qos', '50%', (entity) => {
-        return entity.qos + ' - ' + this.translate.instant(QoSTranslationMap.get(entity.qos));
-      })
+      new EntityTableColumn<RetainedMessage>('qos', 'retained-message.qos', '50%',
+        (entity) => this.translate.instant(WsQoSTranslationMap.get(entity.qos))
+      )
     );
 
     this.entitiesFetchFunction = pageLink => this.retainedMsgService.getRetainedMessages(pageLink)
