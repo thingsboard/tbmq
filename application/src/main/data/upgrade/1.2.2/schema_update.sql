22c157fb72b1bb9d5c6094f5c1982e4f046e7d96
dlandiak <dlandiak@thingsboard.io>
2024-01-29 17:50:59 +0200

    sql file reformat

--- application/src/main/data/upgrade/1.2.2/schema_update.sql
+++ application/src/main/data/upgrade/1.2.2/schema_update.sql
@@ -14,27 +14,23 @@
 -- limitations under the License.
 --
 
-CREATE TABLE IF NOT EXISTS websocket_connection
-(
-    id            uuid         NOT NULL
-        CONSTRAINT websocket_connection_pkey PRIMARY KEY,
-    created_time  bigint       NOT NULL,
-    name          varchar(255) NOT NULL,
-    user_id       uuid,
+CREATE TABLE IF NOT EXISTS websocket_connection (
+    id uuid NOT NULL CONSTRAINT websocket_connection_pkey PRIMARY KEY,
+    created_time bigint NOT NULL,
+    name varchar (255) NOT NULL,
+    user_id uuid,
     configuration jsonb,
-    search_text   varchar(255),
+    search_text varchar (255),
     CONSTRAINT name_unq_key UNIQUE (name),
     CONSTRAINT fk_user_id
-        FOREIGN KEY (user_id) REFERENCES broker_user (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES broker_user (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS websocket_subscription
-(
-    id                      uuid   NOT NULL
-        CONSTRAINT websocket_subscription_pkey PRIMARY KEY,
-    created_time            bigint NOT NULL,
-    websocket_connection_id uuid   NOT NULL,
-    configuration           jsonb,
+CREATE TABLE IF NOT EXISTS websocket_subscription (
+    id uuid NOT NULL CONSTRAINT websocket_subscription_pkey PRIMARY KEY,
+    created_time bigint NOT NULL,
+    websocket_connection_id uuid NOT NULL,
+    configuration jsonb,
     CONSTRAINT fk_websocket_connection_id
-        FOREIGN KEY (websocket_connection_id) REFERENCES websocket_connection (id) ON DELETE CASCADE
+    FOREIGN KEY (websocket_connection_id) REFERENCES websocket_connection (id) ON DELETE CASCADE
 );
