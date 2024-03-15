3c2ad4d1f318d663e6ef5ca488cd692756283d18
dlandiak <dlandiak@thingsboard.io>
2024-03-15 10:48:56 +0200

    rate limits for outgoing PUBLISH messages to non-persistent subscribers with qos=0

--- application/src/test/java/org/thingsboard/mqtt/broker/service/limits/RateLimitServiceImplTest.java
+++ application/src/test/java/org/thingsboard/mqtt/broker/service/limits/RateLimitServiceImplTest.java
@@ -28,7 +28,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.thingsboard.mqtt.broker.actors.client.service.session.ClientSessionService;
 import org.thingsboard.mqtt.broker.common.data.ClientSessionInfo;
 import org.thingsboard.mqtt.broker.common.util.TbRateLimits;
-import org.thingsboard.mqtt.broker.config.RateLimitsConfiguration;
+import org.thingsboard.mqtt.broker.config.IncomingRateLimitsConfiguration;
+import org.thingsboard.mqtt.broker.config.OutgoingRateLimitsConfiguration;
+import org.thingsboard.mqtt.broker.gen.queue.QueueProtos;
 
 import java.util.UUID;
 
@@ -42,7 +44,9 @@ public class RateLimitServiceImplTest {
     private static final String CLIENT_ID = "test";
 
     @MockBean
-    RateLimitsConfiguration rateLimitsConfiguration;
+    IncomingRateLimitsConfiguration incomingRateLimitsConfiguration;
+    @MockBean
+    OutgoingRateLimitsConfiguration outgoingRateLimitsConfiguration;
     @MockBean
     ClientSessionService clientSessionService;
 
@@ -51,45 +55,103 @@ public class RateLimitServiceImplTest {
 
     @Before
     public void setUp() throws Exception {
-        rateLimitService.getClientLimits().put(CLIENT_ID, new TbRateLimits("1:1")); // limit 1 per 1 second
+        when(incomingRateLimitsConfiguration.isEnabled()).thenReturn(true);
+        when(outgoingRateLimitsConfiguration.isEnabled()).thenReturn(true);
+
+        rateLimitService.init();
+
+        rateLimitService.getIncomingPublishClientLimits().put(CLIENT_ID, new TbRateLimits("1:1")); // limit 1 per 1 second
+        rateLimitService.getOutgoingPublishClientLimits().put(CLIENT_ID, new TbRateLimits("1:1")); // limit 1 per 1 second
     }
 
     @After
     public void tearDown() throws Exception {
-        Mockito.reset(rateLimitsConfiguration, clientSessionService);
+        Mockito.reset(incomingRateLimitsConfiguration, outgoingRateLimitsConfiguration, clientSessionService);
     }
 
     @Test
-    public void givenRateLimitsDisabled_whenCheckLimits_thenSuccess() {
-        when(rateLimitsConfiguration.isEnabled()).thenReturn(false);
+    public void givenIncomingRateLimitsDisabled_whenCheckIncomingLimits_thenSuccess() {
+        when(incomingRateLimitsConfiguration.isEnabled()).thenReturn(false);
 
-        boolean result = rateLimitService.checkLimits(CLIENT_ID, UUID.randomUUID(), null);
+        boolean result = rateLimitService.checkIncomingLimits(CLIENT_ID, UUID.randomUUID(), null);
         Assert.assertTrue(result);
     }
 
     @Test
-    public void givenRateLimitsEnabled_whenCheckLimits_thenGetExpectedResult() {
-        when(rateLimitsConfiguration.isEnabled()).thenReturn(true);
+    public void givenIncomingRateLimitsEnabled_whenCheckIncomingLimits_thenGetExpectedResult() {
+        when(incomingRateLimitsConfiguration.isEnabled()).thenReturn(true);
         UUID sessionId = UUID.randomUUID();
 
-        boolean first = rateLimitService.checkLimits(CLIENT_ID, sessionId, null);
+        boolean first = rateLimitService.checkIncomingLimits(CLIENT_ID, sessionId, null);
         Assert.assertTrue(first);
-        boolean second = rateLimitService.checkLimits(CLIENT_ID, sessionId, null);
+        boolean second = rateLimitService.checkIncomingLimits(CLIENT_ID, sessionId, null);
         Assert.assertFalse(second);
-        boolean third = rateLimitService.checkLimits(CLIENT_ID, sessionId, null);
+        boolean third = rateLimitService.checkIncomingLimits(CLIENT_ID, sessionId, null);
         Assert.assertFalse(third);
+    }
+
+    @Test
+    public void givenOutgoingRateLimitsDisabled_whenCheckOutgoingLimits_thenSuccess() {
+        when(outgoingRateLimitsConfiguration.isEnabled()).thenReturn(false);
+
+        boolean result = rateLimitService.checkOutgoingLimits(CLIENT_ID, null);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenOutgoingRateLimitsEnabled_whenCheckOutgoingLimitsWithQos0_thenGetExpectedResult() {
+        when(outgoingRateLimitsConfiguration.isEnabled()).thenReturn(true);
+
+        QueueProtos.PublishMsgProto proto = QueueProtos.PublishMsgProto.newBuilder().setQos(0).build();
+
+        boolean first = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertTrue(first);
+        boolean second = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertFalse(second);
+        boolean third = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertFalse(third);
+    }
+
+    @Test
+    public void givenOutgoingRateLimitsEnabled_whenCheckOutgoingLimitsWithQos1_thenGetExpectedResult() {
+        when(outgoingRateLimitsConfiguration.isEnabled()).thenReturn(true);
+
+        QueueProtos.PublishMsgProto proto = QueueProtos.PublishMsgProto.newBuilder().setQos(1).build();
+
+        boolean first = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertTrue(first);
+        boolean second = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertTrue(second);
+        boolean third = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertTrue(third);
+    }
+
+    @Test
+    public void givenOutgoingRateLimitsEnabled_whenCheckOutgoingLimitsWithQos2_thenGetExpectedResult() {
+        when(outgoingRateLimitsConfiguration.isEnabled()).thenReturn(true);
+
+        QueueProtos.PublishMsgProto proto = QueueProtos.PublishMsgProto.newBuilder().setQos(2).build();
+
+        boolean first = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertTrue(first);
+        boolean second = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertTrue(second);
+        boolean third = rateLimitService.checkOutgoingLimits(CLIENT_ID, proto);
+        Assert.assertTrue(third);
     }
 
     @Test
     public void givenOneClient_whenRemoveIt_thenSuccess() {
         rateLimitService.remove(CLIENT_ID);
-        assertEquals(0, rateLimitService.getClientLimits().size());
+        assertEquals(0, rateLimitService.getIncomingPublishClientLimits().size());
+        assertEquals(0, rateLimitService.getOutgoingPublishClientLimits().size());
     }
 
     @Test
     public void givenOneClient_whenRemoveNull_thenSuccess() {
         rateLimitService.remove(null);
-        assertEquals(1, rateLimitService.getClientLimits().size());
+        assertEquals(1, rateLimitService.getIncomingPublishClientLimits().size());
+        assertEquals(1, rateLimitService.getOutgoingPublishClientLimits().size());
     }
 
     @Test
