503de0b1e9ca4671d5820cb0a48bb0d36fbbb029
deaflynx <artemio.babak@gmail.com>
2024-07-29 17:02:30 +0300

    updated validation and fixed update of custom host/port in connectivity settings

--- ui-ngx/src/app/shared/models/settings.models.ts
+++ ui-ngx/src/app/shared/models/settings.models.ts
@@ -17,7 +17,6 @@
 export const smtpPortPattern: RegExp = /^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/;
 export const connectivitySettingsKey = 'connectivity';
 export const websocketSettingsKey = 'websocket';
-export const securitySettingsKey = 'security';
 
 export interface AdminSettings<T> {
   key: string;
