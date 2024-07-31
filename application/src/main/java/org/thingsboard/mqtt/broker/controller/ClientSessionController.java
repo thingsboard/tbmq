e5e3afabd32eb9089c70aac0530765c1d7362838
dlandiak <dlandiak@thingsboard.io>
2024-07-31 12:48:48 +0300

    return object with credentials name for the getClientSessionCredentials call

--- application/src/main/java/org/thingsboard/mqtt/broker/controller/ClientSessionController.java
+++ application/src/main/java/org/thingsboard/mqtt/broker/controller/ClientSessionController.java
@@ -32,6 +32,7 @@ import org.thingsboard.mqtt.broker.common.data.exception.ThingsboardException;
 import org.thingsboard.mqtt.broker.common.data.page.PageData;
 import org.thingsboard.mqtt.broker.common.data.page.PageLink;
 import org.thingsboard.mqtt.broker.common.data.page.TimePageLink;
+import org.thingsboard.mqtt.broker.dto.ClientSessionCredentialsDto;
 import org.thingsboard.mqtt.broker.dto.ClientSessionStatsInfoDto;
 import org.thingsboard.mqtt.broker.dto.DetailedClientSessionInfoDto;
 import org.thingsboard.mqtt.broker.dto.ShortClientSessionInfoDto;
@@ -170,9 +171,10 @@ public class ClientSessionController extends BaseController {
     @PreAuthorize("hasAnyAuthority('SYS_ADMIN')")
     @RequestMapping(value = "/client-session/credentials", params = {"clientId"}, method = RequestMethod.GET)
     @ResponseBody
-    public String getClientSessionCredentials(@RequestParam String clientId) throws ThingsboardException {
+    public ClientSessionCredentialsDto getClientSessionCredentials(@RequestParam String clientId) throws ThingsboardException {
         try {
-            return checkNotNull(cacheNameResolver.getCache(CacheConstants.CLIENT_SESSION_CREDENTIALS_CACHE).get(clientId, String.class));
+            String credentialsName = checkNotNull(cacheNameResolver.getCache(CacheConstants.CLIENT_SESSION_CREDENTIALS_CACHE).get(clientId, String.class));
+            return new ClientSessionCredentialsDto(credentialsName);
         } catch (Exception e) {
             throw handleException(e);
         }
