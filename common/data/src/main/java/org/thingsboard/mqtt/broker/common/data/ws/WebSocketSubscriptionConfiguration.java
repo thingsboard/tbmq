69d821a9d53867a6533aa929711cc59791100a6f
dlandiak <dlandiak@thingsboard.io>
2024-08-21 16:05:08 +0300

    changed subscriptionId field to Integer

--- common/data/src/main/java/org/thingsboard/mqtt/broker/common/data/ws/WebSocketSubscriptionConfiguration.java
+++ common/data/src/main/java/org/thingsboard/mqtt/broker/common/data/ws/WebSocketSubscriptionConfiguration.java
@@ -32,7 +32,7 @@ public class WebSocketSubscriptionConfiguration {
     @NoXss
     private String color;
     private SubscriptionOptionsDto options;
-    private int subscriptionId;
+    private Integer subscriptionId;
 
     public WebSocketSubscriptionConfiguration(String topicFilter) {
         this(topicFilter, 1);
