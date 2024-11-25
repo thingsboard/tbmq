0d2c10c2b53a05619310d855a5348dde18b660f3
dlandiak <dlandiak@thingsboard.io>
2024-11-25 14:22:32 +0200

    Revert "fix the correct closure of StatefulRedisClusterConnection to prevent WARN io.lettuce.core.RedisChannelHandler - Connection is already closed"
    This reverts commit 4da2abfb7a75213ce46abe81df47827ecba800d6.

--- common/cache/src/main/java/org/thingsboard/mqtt/broker/cache/TBRedisClusterConfiguration.java
+++ common/cache/src/main/java/org/thingsboard/mqtt/broker/cache/TBRedisClusterConfiguration.java
@@ -15,21 +15,15 @@
  */
 package org.thingsboard.mqtt.broker.cache;
 
-import io.lettuce.core.cluster.RedisClusterClient;
-import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import redis.clients.jedis.ConnectionPoolConfig;
 import redis.clients.jedis.DefaultJedisClientConfig;
 import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.UnifiedJedis;
-
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Configuration
 @ConditionalOnProperty(prefix = "redis.connection", value = "type", havingValue = "cluster")
@@ -50,32 +44,6 @@ public class TBRedisClusterConfiguration extends TBRedisCacheConfiguration<Redis
 
     @Value("${redis.password:}")
     private String password;
-
-    @Value("${lettuce.auto-flush:true}")
-    private boolean autoFlush;
-
-    @Bean(destroyMethod = "") // The destroy method is set to nothing on purpose due to the presence of addShutdownHook
-    public StatefulRedisClusterConnection<String, String> statefulRedisClusterConnection(LettuceConnectionFactory lettuceConnectionFactory) {
-        RedisClusterClient client = (RedisClusterClient) lettuceConnectionFactory.getNativeClient();
-
-        if (client == null) {
-            throw new IllegalStateException("Failed to initiate Redis lettuce cluster client!");
-        }
-
-        StatefulRedisClusterConnection<String, String> connection = client.connect();
-        connection.setAutoFlushCommands(autoFlush);
-
-        AtomicBoolean isClosed = new AtomicBoolean(false);
-
-        // Add a custom shutdown hook to ensure single closure
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            if (isClosed.compareAndSet(false, true) && connection.isOpen()) {
-                connection.close();
-            }
-        }));
-
-        return connection;
-    }
 
     @Override
     protected UnifiedJedis loadUnifiedJedis() {
