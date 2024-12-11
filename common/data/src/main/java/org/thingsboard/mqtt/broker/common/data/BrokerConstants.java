0e46176e1b6422aac7805701d379b236ce475051
dlandiak <dlandiak@thingsboard.io>
2024-12-11 12:32:44 +0200

    use sort order in aggregation queries

--- common/data/src/main/java/org/thingsboard/mqtt/broker/common/data/BrokerConstants.java
+++ common/data/src/main/java/org/thingsboard/mqtt/broker/common/data/BrokerConstants.java
@@ -27,6 +27,9 @@ public class BrokerConstants {
     public static final String WS = "WS";
     public static final String WSS = "WSS";
 
+    private final String ASC_ORDER = "ASC";
+    private final String DESC_ORDER = "DESC";
+
     public static final String ENTITY_ID_TOTAL = "total";
     public static final String INCOMING_MSGS = "incomingMsgs";
     public static final String OUTGOING_MSGS = "outgoingMsgs";
