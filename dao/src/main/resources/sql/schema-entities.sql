98e40efa11e12e15edaefe4732bb904114b02209
dlandiak <dlandiak@thingsboard.io>
2024-12-31 12:34:30 +0200

    Set version to 2.1.0-SNAPSHOT

--- dao/src/main/resources/sql/schema-entities.sql
+++ dao/src/main/resources/sql/schema-entities.sql
@@ -27,7 +27,7 @@ BEGIN
     IF (SELECT COUNT(*) FROM tb_schema_settings) = 0 THEN
         INSERT
         INTO tb_schema_settings (schema_version)
-        VALUES (2000001);
+        VALUES (2001000);
     END IF;
 END;
 $$;
