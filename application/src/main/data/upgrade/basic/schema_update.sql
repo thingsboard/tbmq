89d427e67cc173ff58cc7698ac59f30d0acdd040
dlandiak <dlandiak@thingsboard.io>
2025-03-05 18:40:14 +0200

    corrected upgrade by adding missing tables

--- application/src/main/data/upgrade/basic/schema_update.sql
+++ application/src/main/data/upgrade/basic/schema_update.sql
@@ -22,3 +22,47 @@ ALTER TABLE application_shared_subscription DROP COLUMN IF EXISTS search_text;
 ALTER TABLE websocket_connection DROP COLUMN IF EXISTS search_text;
 
 -- UPGRADE FROM VERSION 2.0.0 TO 2.0.1 END
+
+-- UPGRADE FROM VERSION 2.0.1 TO 2.1.0 START
+
+CREATE TABLE IF NOT EXISTS integration (
+    id uuid NOT NULL CONSTRAINT integration_pkey PRIMARY KEY,
+    created_time bigint NOT NULL,
+    disconnected_time bigint,
+    additional_info varchar,
+    configuration varchar(10000000),
+    enabled boolean,
+    name varchar(255),
+    type varchar(255),
+    status varchar
+);
+
+CREATE TABLE IF NOT EXISTS stats_event (
+    id uuid NOT NULL,
+    ts bigint NOT NULL,
+    entity_id uuid NOT NULL,
+    service_id varchar NOT NULL,
+    e_messages_processed bigint NOT NULL,
+    e_errors_occurred bigint NOT NULL
+) PARTITION BY RANGE (ts);
+
+CREATE TABLE IF NOT EXISTS lc_event (
+    id uuid NOT NULL,
+    ts bigint NOT NULL,
+    entity_id uuid NOT NULL,
+    service_id varchar NOT NULL,
+    e_type varchar NOT NULL,
+    e_success boolean NOT NULL,
+    e_error varchar
+) PARTITION BY RANGE (ts);
+
+CREATE TABLE IF NOT EXISTS error_event (
+    id uuid NOT NULL,
+    ts bigint NOT NULL,
+    entity_id uuid NOT NULL,
+    service_id varchar NOT NULL,
+    e_method varchar NOT NULL,
+    e_error varchar
+) PARTITION BY RANGE (ts);
+
+-- UPGRADE FROM VERSION 2.0.1 TO 2.1.0 END
