807a12416fae75253859c3109af7897d3751d40d
dlandiak <dlandiak@thingsboard.io>
2025-04-01 13:43:55 +0300

    correct ClasspathSuite for integration tests

--- application/src/test/java/org/thingsboard/mqtt/broker/service/testing/integration/IntegrationTestSuite.java
+++ application/src/test/java/org/thingsboard/mqtt/broker/service/testing/integration/IntegrationTestSuite.java
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(ClasspathSuite.class)
 @ClasspathSuite.ClassnameFilters({
-        "org.thingsboard.mqtt.broker.service.integration.*TestCase",
+        "org.thingsboard.mqtt.broker.service.testing.integration.*TestCase",
 })
 public class IntegrationTestSuite {
 
