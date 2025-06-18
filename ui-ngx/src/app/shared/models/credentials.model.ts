0e65f00e2abe343565a624d0130409cf00f427b7
deaflynx <artemio.babak@gmail.com>
2025-06-18 15:27:22 +0300

    UI: Remove auth provider types from client credentials model.

--- ui-ngx/src/app/shared/models/credentials.model.ts
+++ ui-ngx/src/app/shared/models/credentials.model.ts
@@ -26,17 +26,13 @@ import {
 } from '@core/utils';
 import { TimePageLink } from '@shared/models/page/page-link';
 
-// TODO combine with MqttAuthProviderType after backend refactoring
 export enum CredentialsType {
   MQTT_BASIC = 'MQTT_BASIC',
   SSL = 'SSL',
   SCRAM = 'SCRAM',
-  JWT = 'JWT',
-  HTTP_SERVICE = 'HTTP_SERVICE'
 }
 
-export const CredentialsTypes = Object.values(CredentialsType)
-  .filter(el => el !== CredentialsType.JWT && el !== CredentialsType.HTTP_SERVICE);
+export const CredentialsTypes = Object.values(CredentialsType);
 
 export const ANY_CHARACTERS = '.*';
 export const wsSystemCredentialsName = 'TBMQ WebSockets MQTT Credentials';
@@ -46,8 +42,6 @@ export const credentialsTypeTranslationMap = new Map<CredentialsType, string>(
     [CredentialsType.MQTT_BASIC, 'mqtt-client-credentials.type-basic'],
     [CredentialsType.SSL, 'mqtt-client-credentials.type-ssl'],
     [CredentialsType.SCRAM, 'mqtt-client-credentials.type-scram'],
-    [CredentialsType.JWT, 'authentication.type-jwt'],
-    [CredentialsType.HTTP_SERVICE, 'authentication.type-http-service'],
   ]
 );
 
