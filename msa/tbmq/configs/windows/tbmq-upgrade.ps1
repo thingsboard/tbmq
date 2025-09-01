485775de506db0e32963ba0d7fd634cefa4cdc42
dlandiak <dlandiak@thingsboard.io>
2025-09-01 13:09:51 +0300

    update to 2.2.0 version for installation and upgrade scripts

--- msa/tbmq/configs/windows/tbmq-upgrade.ps1
+++ msa/tbmq/configs/windows/tbmq-upgrade.ps1
@@ -40,8 +40,8 @@ try {
     Write-Host "Docker Compose version is: $COMPOSE_VERSION"
 
     # Define TBMQ versions
-    $old_version = "2.0.1"
-    $new_version = "2.1.0"
+    $old_version = "2.1.0"
+    $new_version = "2.2.0"
 
     # Define TBMQ images
     $old_image = "image: `"thingsboard/tbmq:$old_version`""
