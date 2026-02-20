4a10429cc07873fd5fd3524ad9f7adab4f459101
dlandiak <dlandiak@thingsboard.io>
2026-02-20 13:18:00 +0200

    added AbstractTbEntityService

---
+++ application/src/main/java/org/thingsboard/mqtt/broker/service/entity/AbstractTbEntityService.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright © 2016-2026 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.mqtt.broker.service.entity;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.thingsboard.mqtt.broker.common.data.exception.ThingsboardErrorCode;
+import org.thingsboard.mqtt.broker.common.data.exception.ThingsboardException;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Slf4j
+public abstract class AbstractTbEntityService {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${server.log_controller_error_stack_trace}")
+    @Getter
+    private boolean logControllerErrorStackTrace;
+
+    protected boolean isTestProfile() {
+        return Set.of(this.env.getActiveProfiles()).contains("test");
+    }
+
+    protected <T> T checkNotNull(T reference) throws ThingsboardException {
+        return checkNotNull(reference, "Requested item wasn't found!");
+    }
+
+    protected <T> T checkNotNull(T reference, String notFoundMessage) throws ThingsboardException {
+        if (reference == null) {
+            throw new ThingsboardException(notFoundMessage, ThingsboardErrorCode.ITEM_NOT_FOUND);
+        }
+        return reference;
+    }
+
+    protected <T> T checkNotNull(Optional<T> reference) throws ThingsboardException {
+        return checkNotNull(reference, "Requested item wasn't found!");
+    }
+
+    protected <T> T checkNotNull(Optional<T> reference, String notFoundMessage) throws ThingsboardException {
+        if (reference.isPresent()) {
+            return reference.get();
+        } else {
+            throw new ThingsboardException(notFoundMessage, ThingsboardErrorCode.ITEM_NOT_FOUND);
+        }
+    }
+
+}
